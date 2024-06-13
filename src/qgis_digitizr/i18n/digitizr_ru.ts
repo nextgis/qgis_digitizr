90a14aa01c25aa8e61797798a0f6d84058578c1d
Valeriy Remeshevskiy <mr.remeshevskiy@mail.com>
2024-06-13 20:45:09 +0300

    Edit catalogue name

---
+++ src/qgis_digitizr/i18n/digitizr_ru.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>AboutDialogBase</name>
+    <message>
+        <location filename="../about_dialog_base.ui" line="14"/>
+        <source>About {plugin_name}</source>
+        <translation>О {plugin_name}</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog_base.ui" line="27"/>
+        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
+        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog_base.ui" line="40"/>
+        <source>&lt;p&gt;{description}&lt;/p&gt;
+&lt;p&gt;{about}&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Developers:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Video with an overview of the plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
+{video_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Please report bugs at&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;Other helpful services by NextGIS:
+&lt;ul&gt;&lt;li&gt;&lt;b&gt;Convenient up-to-date data extracts for any place in the world:
+&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
+&lt;li&gt;&lt;b&gt;Fully featured Web GIS service:
+&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
+{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
+&lt;/p&gt;</source>
+        <translation>&lt;p&gt;{description}&lt;/p&gt;
+&lt;p&gt;{about}&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Разработчики:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Домашняя страница:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Видео с обзором плагина:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
+{video_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Пожалуйста, сообщайте о багах в&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;багтрекер&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;Другие полезные сервисы NextGIS:
+&lt;ul&gt;&lt;li&gt;&lt;b&gt;Удобная выборка актуальных данных из любой точки мира:
+&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
+&lt;li&gt;&lt;b&gt;Полнофункциональный веб-ГИС-сервис:
+&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
+{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
+&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>DigitizrPlugin</name>
+    <message>
+        <location filename="../plugin.py" line="127"/>
+        <source>Add line buffer</source>
+        <translation>Добавить линейный буфер</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="165"/>
+        <source>About plugin…</source>
+        <translation>О плагине…</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="178"/>
+        <source>Buffer size (meters)</source>
+        <translation>Размер буфера (в метрах)</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="222"/>
+        <source>Round</source>
+        <translation>Скруглённые</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="199"/>
+        <source>Square</source>
+        <translation>Квадратные</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="204"/>
+        <source>Flat</source>
+        <translation>Плоские</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="227"/>
+        <source>Miter</source>
+        <translation>Острые</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="232"/>
+        <source>Bevel</source>
+        <translation>Скошенные</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="143"/>
+        <source>End cap style</source>
+        <translation>Стиль торцов</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="150"/>
+        <source>Join style</source>
+        <translation>Стиль соединения</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="190"/>
+        <source>The end cap style parameter controls how line endings are handled in the buffer</source>
+        <translation>Параметр &quot;Стиль торцов&quot; управляет тем, как окончания линии обрабатываются в буфере</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="218"/>
+        <source>The join style parameter controls how segments joins are handled when offsetting corners in a line</source>
+        <translation>Параметр &quot;Стиль соединения&quot; определяет, как обрабатываются соединения сегментов при смещении углов линии</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="97"/>
+        <source>{plugin_name} Toolbar</source>
+        <translation>Инструменты {plugin_name}</translation>
+    </message>
+</context>
+</TS>
---
+++ src/qgis_digitizr/qgsmaptooladdlinebuffer.py
@@ -0,0 +1,164 @@
+from typing import Optional, cast
+
+from qgis.core import (
+    Qgis,
+    QgsCoordinateTransform,
+    QgsDistanceArea,
+    QgsFeature,
+    QgsGeometry,
+    QgsProject,
+    QgsVectorDataProvider,
+    QgsVectorLayer,
+    QgsWkbTypes,
+)
+from qgis.gui import (
+    QgisInterface,
+    QgsAdvancedDigitizingDockWidget,
+    QgsMapCanvas,
+    QgsMapMouseEvent,
+    QgsMapToolCapture,
+)
+from qgis.PyQt.QtCore import Qt, pyqtSignal
+from qgis.PyQt.QtWidgets import QAction
+from qgis.utils import iface
+
+iface = cast(QgisInterface, iface)
+
+
+class QgsMapToolAddLineBuffer(QgsMapToolCapture):
+    availabilityChanged = pyqtSignal(bool)
+
+    __buffer_size: float
+    __cap_style: Qgis.EndCapStyle
+    __join_style: Qgis.JoinStyle
+
+    def __init__(
+        self,
+        canvas: QgsMapCanvas,
+        cadDockWidget: QgsAdvancedDigitizingDockWidget,
+    ) -> None:
+        super().__init__(
+            canvas, cadDockWidget, QgsMapToolCapture.CaptureMode.CaptureLine
+        )
+
+        self.__buffer_size = 10.0
+        self.__cap_style = Qgis.EndCapStyle.Round
+        self.__join_style = Qgis.JoinStyle.Round
+
+        canvas.currentLayerChanged.connect(self.checkAvailability)
+
+        main_window = iface.mainWindow()
+        assert main_window is not None
+        add_feature_action = main_window.findChild(
+            QAction, "mActionToggleEditing"
+        )
+        assert add_feature_action is not None
+        add_feature_action.toggled.connect(
+            self.checkAvailability,
+            type=Qt.ConnectionType.QueuedConnection,  # type: ignore
+        )
+
+    def set_buffer_size(self, size: float) -> None:
+        self.__buffer_size = size
+
+    def set_cap_style(self, cap_style: Qgis.EndCapStyle) -> None:
+        self.__cap_style = cap_style
+
+    def set_join_style(self, join_style: Qgis.JoinStyle) -> None:
+        self.__join_style = join_style
+
+    def convert_distance(self):
+        project = QgsProject.instance()
+        assert project is not None
+        dist_calculator = QgsDistanceArea()
+        dist_calculator.setSourceCrs(project.crs(), project.transformContext())
+        dist_calculator.setEllipsoid(project.ellipsoid())
+        result = dist_calculator.convertLengthMeasurement(
+            self.__buffer_size, project.crs().mapUnits()
+        )
+        return result
+
+    def cadCanvasReleaseEvent(self, event: Optional[QgsMapMouseEvent]) -> None:
+        assert event is not None
+
+        vlayer = self.currentVectorLayer()
+
+        if vlayer is None:
+            self.notifyNotVectorLayer()
+            return
+        if not vlayer.isEditable():
+            self.notifyNotEditableLayer()
+            return
+
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.addVertex(event.mapPoint(), event.mapPointMatch())
+            # TODO Process error
+            self.startCapturing()
+            return
+        elif event.button() != Qt.MouseButton.RightButton:
+            self.deleteTempRubberBand()
+            return
+
+        line_wkt = self.captureCurve().curveToLine().asWkt()
+        self.stopCapturing()
+
+        line_geometry = QgsGeometry.fromWkt(line_wkt)
+        transform = QgsCoordinateTransform(
+            vlayer.crs(),
+            self.canvas().mapSettings().destinationCrs(),
+            QgsProject.instance(),
+        )
+
+        lenght_buffer = self.convert_distance()
+
+        line_geometry.transform(transform)
+        buffer_geometry = line_geometry.buffer(
+            distance=lenght_buffer,
+            segments=10,
+            endCapStyle=self.__cap_style,
+            joinStyle=self.__join_style,
+            miterLimit=2,
+        )
+
+        buffer_geometry.transform(
+            transform, QgsCoordinateTransform.ReverseTransform
+        )
+
+        f = QgsFeature(vlayer.fields())
+        f.setGeometry(buffer_geometry)
+
+        vlayer.beginEditCommand("Add line buffer")
+        vlayer.addFeature(f)
+        vlayer.endEditCommand()
+        self.canvas().refresh()
+
+    def checkAvailability(self):
+        self.availabilityChanged.emit(self.isAvalable())
+
+    def isAvalable(self):
+        current_layer = self.currentVectorLayer()
+
+        if current_layer is None:
+            return False
+
+        if not current_layer.isEditable():
+            return False
+
+        if not self.__is_layer_suitable(current_layer):
+            return False
+
+        return True
+
+    def __is_layer_suitable(self, layer: QgsVectorLayer) -> bool:
+        PolygonGeometry = QgsWkbTypes.GeometryType.PolygonGeometry
+        if layer.geometryType() != PolygonGeometry:
+            return False
+
+        data_provider = layer.dataProvider()
+        if data_provider is None:
+            return False
+
+        Capability = QgsVectorDataProvider.Capability
+        if not bool(data_provider.capabilities() & Capability.AddFeatures):
+            return False
+        return True
