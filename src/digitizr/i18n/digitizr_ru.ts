a0031b5bb44f9bff30a861a35154b7c3fc44235a
Ivan Barsukov <i.barsukov@protonmail.com>
2024-06-21 22:02:49 +0700

    Update about dialog

--- src/digitizr/i18n/digitizr_ru.ts
+++ src/digitizr/i18n/digitizr_ru.ts
@@ -2,6 +2,54 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU" sourcelanguage="en_US">
 <context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../about_dialog.py" line="83"/>
+        <source>Developers</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="84"/>
+        <source>Homepage</source>
+        <translation>Домашняя страница</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="85"/>
+        <source>Please report bugs at</source>
+        <translation>Пожалуйста, сообщайте о багах в</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="88"/>
+        <source>Video with an overview of the plugin</source>
+        <translation>Видео с обзором на плагин</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="89"/>
+        <source>Other helpful services by NextGIS</source>
+        <translation>Другие полезные сервисы от NextGIS</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="90"/>
+        <source>Convenient up-to-date data extracts for any place in the world</source>
+        <translation>Удобная выборка актуальных данных из любой точки мира</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="93"/>
+        <source>Fully featured Web GIS service</source>
+        <translation>Полнофункциональный Веб ГИС-сервис</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="87"/>
+        <source>bugtracker</source>
+        <translation>багтрекер</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="78"/>
+        <source>REPORT_END</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
     <name>AboutDialogBase</name>
     <message>
         <location filename="../about_dialog_base.ui" line="14"/>
@@ -13,102 +61,71 @@
         <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
         <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
     </message>
-    <message>
-        <location filename="../about_dialog_base.ui" line="40"/>
-        <source>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Developers:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Video with an overview of the plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Please report bugs at&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Other helpful services by NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Convenient up-to-date data extracts for any place in the world:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Fully featured Web GIS service:
-&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
-{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</source>
-        <translation>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Разработчики:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Домашняя страница:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Видео с обзором плагина:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Пожалуйста, сообщайте о багах в&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;багтрекер&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Другие полезные сервисы NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Удобная выборка актуальных данных из любой точки мира:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Полнофункциональный веб-ГИС-сервис:
-&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
-{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</translation>
-    </message>
 </context>
 <context>
     <name>DigitizrPlugin</name>
     <message>
-        <location filename="../plugin.py" line="127"/>
+        <location filename="../plugin.py" line="132"/>
         <source>Add line buffer</source>
         <translation>Добавить линейный буфер</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="165"/>
+        <location filename="../plugin.py" line="168"/>
         <source>About plugin…</source>
         <translation>О плагине…</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="178"/>
+        <location filename="../plugin.py" line="181"/>
         <source>Buffer size (meters)</source>
         <translation>Размер буфера (в метрах)</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="222"/>
+        <location filename="../plugin.py" line="229"/>
         <source>Round</source>
         <translation>Скруглённые</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="199"/>
+        <location filename="../plugin.py" line="204"/>
         <source>Square</source>
         <translation>Квадратные</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="204"/>
+        <location filename="../plugin.py" line="209"/>
         <source>Flat</source>
         <translation>Плоские</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="227"/>
+        <location filename="../plugin.py" line="234"/>
         <source>Miter</source>
         <translation>Острые</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="232"/>
+        <location filename="../plugin.py" line="239"/>
         <source>Bevel</source>
         <translation>Скошенные</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="143"/>
+        <location filename="../plugin.py" line="148"/>
         <source>End cap style</source>
         <translation>Стиль торцов</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="150"/>
+        <location filename="../plugin.py" line="153"/>
         <source>Join style</source>
         <translation>Стиль соединения</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="190"/>
+        <location filename="../plugin.py" line="193"/>
         <source>The end cap style parameter controls how line endings are handled in the buffer</source>
         <translation>Параметр &quot;Стиль торцов&quot; управляет тем, как окончания линии обрабатываются в буфере</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="218"/>
+        <location filename="../plugin.py" line="223"/>
         <source>The join style parameter controls how segments joins are handled when offsetting corners in a line</source>
         <translation>Параметр &quot;Стиль соединения&quot; определяет, как обрабатываются соединения сегментов при смещении углов линии</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="97"/>
+        <location filename="../plugin.py" line="101"/>
         <source>{plugin_name} Toolbar</source>
         <translation>Инструменты {plugin_name}</translation>
     </message>
